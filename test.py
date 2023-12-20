387f7a10ccdd6fc437b39774dc1d7e3e87ad9623
iraaz4321 <69959972+iraaz4321@users.noreply.github.com>
2023-12-20 15:42:16 +0200

    few small fixes

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 json_data = {"amount": 0.0001, "redirect_url": "https://google.com/", "receiver": "iraas"}
-r = requests.post("http://127.0.0.1:8000/api/payment/", json=json_data)
+r = requests.post("https://bluecoin.iraas.xyz/api/payment/", json=json_data)
 
 #json_data = {"account": "IzC3c4lawTtvNjin"}
 #r = requests.post("http://127.0.0.1:8000/api/payment_state/", json=json_data)
